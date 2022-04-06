4c3066a1c197535ce3bed8ae167ec12c828834a1
Ibn-Maged <mohamedmaged1607@gmail.com>
2022-04-06 02:05:33 +0200

    added the invert filter and rotation filter

--- Main V1.0.cpp
+++ Main V1.0.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cstring>
 #include "bmplib.h"
 #include "bmplib.cpp"
 
 using namespace std;
 
-void filter1();
-void filter2();
+void invert();
+void rotate();
 void filter3();
 void filter4();
 void filter5();
@@ -14,25 +15,25 @@ void load_image();
 void save_image();
 
 unsigned char image_grid[SIZE][SIZE];
-
+unsigned char new_image_grid[SIZE][SIZE];
 
 int main()
 {
     int choice;
     cout << "Hello Dear User!\n" << "what Do You Need Today?\n";
-    cout << "1- Filter 1\n" << "2- Filter 2\n" << "3- Filter 3\n" << "4- Filter 4\n" << "5- Filter 5\n" << "6- Filter 6" << endl;
+    cout << "1- Invert \n" << "2- Rotate \n" << "3- Filter 3\n" << "4- Filter 4\n" << "5- Filter 5\n" << "6- Filter 6" << endl;
     cin >> choice;
     cin.clear();
     cin.sync();
 
     if (choice == 1)
     {
-        filter1();
+        invert();
     }
     
     else if (choice == 2)
     {
-        filter2();
+        rotate();
     }
     
     else if (choice == 3)
@@ -56,13 +57,70 @@ int main()
     }
 }
 
-void filter1()
+void invert()
 {
-    cout << "filter 1 test";
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            image_grid[i][j] = 255 - image_grid[i][j];
+        }
+    }
 }
-void filter2()
+void rotate()
 {
-    cout << "filter 2 test";
+    int choice;
+    cout << "Do You Want Rotate by 90 or 180 or 270? ";
+    cin >> choice;
+    
+    if (choice == 90)
+    {
+        for (int i =0; i < SIZE - 1; i++)           // copy from the image grid to a new grid 
+        {
+            for (int j = 0; j < SIZE - 1; j++)
+            {
+                new_image_grid[j][i] = image_grid[SIZE-i-1][j];
+            }
+        }
+ 
+        for (int i = 0; i < SIZE - 1; i++)         // returning the new grid to the old one 
+        {
+            for (int j = 0; j < SIZE - 1; j++)
+            {
+                image_grid[i][j] = new_image_grid[i][j];
+            }
+        }
+            
+    }
+    else if (choice == 180)
+    {
+        for (int i = 0; i < (SIZE/2)-1; i++)
+        {
+            for (int j = 0; j < SIZE-1 ; j++)
+            {
+                swap(image_grid[i][j] , image_grid[SIZE-1-i][j]);
+            }
+        }
+    }
+    else if (choice == 270)
+    {
+        for (int i = 0; i < SIZE - 1; i++)
+        {
+            for (int j = 0; j < SIZE - 1; j++)
+            {
+                new_image_grid[j][i] = image_grid[i][SIZE-j-1];
+            }
+        }
+
+        for (int i = 0; i < SIZE - 1; i++)          
+        {
+            for (int j = 0; j < SIZE - 1; j++)
+            {
+                image_grid[i][j] = new_image_grid[i][j];
+            }
+        }
+
+    }
 }
 void filter3()
 {
